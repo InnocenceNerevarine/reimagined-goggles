16359db5fbc780bb3fb5e40312e89117ae906a73
Илья <ilya.sidorov.2014@gmail.com>
2020-05-08 13:26:36 +0300

    Экзамен мдк 03_02, оптимизированная версия (нет)

---
+++ Load_CPU_RAM/Load_CPU_RAM/ClassRAM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections; // Для работы с листами
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Load_CPU_RAM
+{
+    class ClassRAM
+    {
+        // Накопительный массив заполняющий память
+        public static ArrayList arrayList = new ArrayList();
+
+       
+    
+        
+        // Положить объект "pattern" в лист.
+        public static void FillMemory()
+        {
+            lock (arrayList)
+            {
+                ClassPattern @pattern = new ClassPattern();
+
+                arrayList.Add(value: @pattern);
+            }
+
+   
+        }
+    }
+}
